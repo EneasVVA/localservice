01cb86cd38fe2ea7ccda34b2ef4985e302d956e5
deshpande.koustubha <deshpande.koustubha@gmail.com>
2010-10-07 23:01:11 +0000

    Adding android DB project

---
+++ AndroidDBEvents/src/com/example/events/Events.java
@@ -0,0 +1,31 @@
+package com.example.events;
+
+import static android.provider.BaseColumns._ID;
+import static com.example.events.Constants.TABLE_NAME;
+import static com.example.events.Constants.TIME;
+import static com.example.events.Constants.TITLE;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Events extends SQLiteOpenHelper {
+	public Events(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+	private static final String DATABASE_NAME = "events.db";
+	private static final int DATABASE_VERSION = 1;
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("CREATE TABLE " + TABLE_NAME + " (" + _ID
+				+ " INTEGER PRIMARY KEY AUTOINCREMENT, " + TIME + " INTEGER,"
+				+ TITLE + " TEXT NOT NULL);");
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+		onCreate(db);
+	}
+
+}
